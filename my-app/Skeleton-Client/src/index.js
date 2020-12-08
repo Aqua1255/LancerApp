e7aaeb75b8940e6ad3c66e7ac234bedf8bca97de
Alex Quinn <alexbbquinn@Genesiss-MacBook-Pro.local>
2020-12-08 15:28:34 -0800

    g2g

--- my-app/Skeleton-Client/src/index.js
+++ my-app/Skeleton-Client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SetUpProxy from './SetUp-Proxy';
+import SetUpProxy from './SetUp-Proxy.tsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
