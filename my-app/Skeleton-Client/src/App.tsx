b9dba71c87ce14c268e13a4400fdca3793345d88
Alex Quinn <alexbbquinn@Genesiss-MacBook-Pro.local>
2020-12-13 22:55:40 -0800

    looking good, need to increase length on form fields and then pass tests on blue 7

--- my-app/Skeleton-Client/src/App.tsx
+++ my-app/Skeleton-Client/src/App.tsx
@@ -38,125 +38,132 @@ export const App = () => {
 
     return (
 
-            <div className="container">
+            <div className="background-container">
                 <header className="App-header">
                     <h1 className="body">
                         Lancer Brigade Operations Center
                     </h1>
                 </header>
                 <img src={Logo} className="App-logo" alt="logo"/>
-                <form>
-                    <div>
-                        <label className="label-row">
-                            Reporting Date Time Group:
-                            <input value = {reportingDateInputText} onChange={(event) => {
-                                reportingDateSetInputText(event.target.value)
-                            }} />
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                            Unit & Call Sign:
-                            <input value={callSignInputText} onChange={(event) => {
-                                callSignSetInputText(event.target.value)
-                            }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                            SP Date Time Group:
-                            <input value={spInputText} onChange={(event) => {
-                                spSetInputText(event.target.value)
-                            }}/>
-                        </label>
-                    </div>
 
-                    <div>
-                        <label className="label-row">
-                        Estimated RP Date Time Group:
-                        <input value={rpInputText} onChange={(event) => {
-                            rpSetInputText(event.target.value)
-                        }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                        Command Post Location:
-                        <input value={cpInputText} onChange={(event) => {
-                            cpSetInputText(event.target.value)
-                        }}/>
-                        </label >
-                    </div>
-                    <div>
-                        <label className="label-row">
-                        ETA to Continue Operations:
-                        <input value={etaInputText} onChange={(event) => {
-                            etaSetInputText(event.target.value)
-                        }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                        Sensitive Items Status:
-                        <input value={sensitiveInputText} onChange={(event) => {
-                            sensitiveSetInputText(event.target.value)
-                        }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                        One Sentence Narrative:
-                        <input value={narInputText} onChange={(event) => {
-                            narSetInputText(event.target.value)
-                        }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label className="label-row">
-                        Contact Name and Phone #:
-                        <input value={contactInputText} onChange={(event) => {
-                            contactSetInputText(event.target.value)
-                        }}/>
-                        </label>
-                    </div>
+                <div className="form-container">
+                    <form>
+                        <div>
+                            <label className="label-row">
+                                Reporting Date Time Group:
+                                <input value={reportingDateInputText} onChange={(event) => {
+                                    reportingDateSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                Unit & Call Sign:
+                                <input value={callSignInputText} onChange={(event) => {
+                                    callSignSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                SP Date Time Group:
+                                <input value={spInputText} onChange={(event) => {
+                                    spSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
 
-                    <button onClick={() => alert(reportingDateInputText + callSignInputText + spInputText
-                        + rpInputText + cpInputText + etaInputText + sensitiveInputText + narInputText +
-                        contactInputText)
-                    }>check in</button>
-                </form>
-                <form>
-                    <div>
-                        <label className="label-row">
-                            Reporting Date Time Group:
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            Unit & Call Sign
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            RP Date Time Group
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            Command Post Location
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            Accidents or SIGACTs
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            ETA to Continue Operations
-                            <input/>
-                        </label>
-                        <label className="label-row">
-                            Sensitive Item Status
-                            <input/>
-                        </label>
-
-                    </div>
-                </form>
+                        <div>
+                            <label className="label-row">
+                                Estimated RP Date Time Group:
+                                <input value={rpInputText} onChange={(event) => {
+                                    rpSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                Command Post Location:
+                                <input value={cpInputText} onChange={(event) => {
+                                    cpSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                ETA to Continue Operations:
+                                <input value={etaInputText} onChange={(event) => {
+                                    etaSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                Sensitive Items Status:
+                                <input value={sensitiveInputText} onChange={(event) => {
+                                    sensitiveSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                One Sentence Narrative:
+                                <input value={narInputText} onChange={(event) => {
+                                    narSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="label-row">
+                                Contact Name and Phone #:
+                                <input value={contactInputText} onChange={(event) => {
+                                    contactSetInputText(event.target.value)
+                                }}/>
+                            </label>
+                        </div>
+                        <div className="checkButton-styling">
+                        <button onClick={() => alert(reportingDateInputText + callSignInputText + spInputText
+                            + rpInputText + cpInputText + etaInputText + sensitiveInputText + narInputText +
+                            contactInputText)
+                        }>check in
+                        </button>
+                        </div>
+                    </form>
+                    <form>
+                        <div>
+                            <label className="label-row">
+                                Reporting Date Time Group:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                Unit & Call Sign:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                RP Date Time Group:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                Command Post Location:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                Accidents or SIGACTs:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                ETA to Continue Operations:
+                                <input/>
+                            </label>
+                            <label className="label-row">
+                                Sensitive Item Status:
+                                <input/>
+                            </label>
+                        </div>
+                        <div className="checkButton-styling">
+                        <button>check out</button>
+                        </div>
+                    </form>
+                </div>
         </div>
     )}
